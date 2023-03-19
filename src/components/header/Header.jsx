6c05cc0d2e1ee1f73c1bac1cd9a31dd0a6514ac8
tanishka1306 <taniskha.rajpoot_cs20@gla.ac.in>
2023-03-20 02:00:25 +0530

    updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-     <div >
-            <div className=''>
-            {/* <img
-          src="./Images/headerimg.jpg"
-          className=" w-full  opacity-50 "
-          alt=""
-        /> */}
-            </div>
-     </div>
+        <div className="">
+          <img src="./Images/headerimg.jpg" className="opacity-50 h-[800px]" alt="" />
+        </div>
+         
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,40 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom'
-import Button from "./btn";
+// import Button from "./btn";
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
 
+    const handelNav = () => {
+      setNav(!nav) 
+    }
   return (
    
-          <div>
-    <div className="shadow-md w-full fixed top-0 left-0 bg-red-50 opacity-1"> 
-    <div className="italic font-bold text-2xl cursor-pointer py-4 md:px-10 px-7 lg:float-left flex items-center font-[Poppins] text-red-400"> 
-             SAMAROH
-             </div>
-          <div className="md:flex items-center lg:float-right justify-between flex-row py-4 md:px-10 px-7">
-          
-                 <ul className="md:flex md:items-center md:pb-0 pb-12 absolute md:static float-right bg-red-50 md:z-auto z-[-1] left-0 w-full ms:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in " >
-                
-                    <li className="md:ml-8 text-xl md:my-0 my-7 "> <Link className="text-red-400 hover:text-black duration-300" to="/home"  >Home</Link></li>
-                    <li className="md:ml-8 text-xl md:my-0 my-7">  <Link className="text-red-400 hover:text-black duration-300"  to="/contactus"  >Contact Us</Link> </li>
-                    <li className="md:ml-8 text-xl md:my-0 my-7"> <Link className="text-red-400 hover:text-black duration-300"  to="/contactus"  > See our Work</Link></li>
-                    <li className="md:ml-8 text-xl md:my-0 my-7"> <Link className="text-red-400 hover:text-black duration-300"  to="/contactus"  >About Us</Link></li>
-                    <Button className="float-right sm:mr-4">
-                      Register
-                    </Button>
-                    <Button>
-                      Login
-                    </Button>
-                 </ul>
-      
-      
-          </div>
-
-    </div>
-
-
-
+  <div>
+      <div 
+         className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-rose-900">
+            <h1 className="w-full text-4xl  ">
+              S A M A R O H
+            </h1>
+            <ul className="hidden md:flex" >
+                    <li className="p-4 text-2xl"> <Link className="text-red-900 hover:text-black duration-300" to="/home"  >Home</Link></li>
+                    <li className="p-4 text-2xl">  <Link className="text-red-900 hover:text-black duration-300"  to="/contactus"  >Contact</Link> </li>
+                    <li className="p-4 text-2xl"> <Link className="text-red-900 hover:text-black duration-300"  to="/contactus"  > Gallery</Link></li>
+                    <li className="p-4 text-2xl"> <Link className="text-red-900 hover:text-black duration-300"  to="/contactus"  >About</Link></li>
+            </ul>
+            <div onClick={handelNav} className="block md:hidden">
+              {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+             
+            </div>
+            <div className={!nav ? "fixed left-0 top-0 w-[60%] border-r h-full bg-[#F5EBEB] border-r-rose-200 ease-in-out duration-700" : "fixed left-[-100%]" }>  
+                <h1 className="w-full text-4xl m-4">
+                   S A M A R O H
+                </h1>
+              <ul className="pt-4 uppercase" >
+                    <li className="p-4 border-b border-rose-300 font-bold text-[10px]"> <Link className="text-red-900 hover:text-black duration-300" to="/home"  >Home</Link></li>
+                    <li className="p-4 border-b border-rose-300 font-bold  text-[10px]">  <Link className="text-red-900 hover:text-black duration-300"  to="/contactus"  >Contact</Link> </li>
+                    <li className="p-4 border-b border-rose-300 font-bold  text-[10px]"> <Link className="text-red-900 hover:text-black duration-300"  to="/contactus"  > Gallery</Link></li>
+                    <li className="p-4 font-bold text-[10px]"> <Link className="text-red-900 hover:text-black duration-300"  to="/contactus"  >About</Link></li>
+              </ul>
+            </div>
+        </div>
+    
   </div>
    
   );
